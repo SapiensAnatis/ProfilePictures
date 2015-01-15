b23145bb844b36c9f5a38bead719be3059375088
revalationist <jay0@me.com>
2015-01-15 16:30:45 +0000

    Edit

--- shared/settings.lua
+++ shared/settings.lua
@@ -1,7 +1,7 @@
 GlobalSettings = {}
 
 GlobalSettings.Timeout = 5 -- The number of seconds after which the script should give up trying to ping Steam servers and instead use the default avatar
-GlobalSettings.APIKey = "075AB53DC16E31E66B5D10A61D3766CC" -- Since under the ToS of the API agreement I'm not allowed to share an API key (and I'm sure my requests would go over 1000 per day if I shared it anyway), you must enter yours here. Get one @ http://steamcommunity.com/dev/apikey
+GlobalSettings.APIKey = "" -- Since under the ToS of the API agreement I'm not allowed to share an API key (and I'm sure my requests would go over 1000 per day if I shared it anyway), you must enter yours here. Get one @ http://steamcommunity.com/dev/apikey
 
 
 GlobalSettings.StoreB64OnServer = true
